004919fe36d0f47acdac3aca4c1249aeee38da56
avnishkumar <avnish.kumar@corpseed.com>
2026-02-16 14:39:37 +0530

    some fixes and changes

--- app/service/ServiceFaqs.jsx
+++ app/service/ServiceFaqs.jsx
@@ -52,7 +52,7 @@ export default function ServiceFaqs({
                   </span>
                 </summary>
 
-                <div className="px-5 pb-5 pt-0 text-sm sm:text-base text-gray-700 leading-relaxed bg-gray-100">
+                <div className="px-5 pb-5 pt-0 text-sm sm:text-base text-gray-700 leading-relaxed bg-gray-300">
                   {/* ✅ answer is HTML from API */}
                   <SafeHtmlShadow html={item.answerHtml} />
                 </div>

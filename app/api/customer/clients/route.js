23715b2ff3095412d5f0ef8d3e0963da4e906b21
avnishkumar <avnish.kumar@corpseed.com>
2026-02-23 19:02:24 +0530

    some minor changes

--- app/api/customer/clients/route.js
+++ app/api/customer/clients/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const res = await fetch(
-    "http://www.admin.corpseed.com/api/customer/clients",
+    "https://admin.corpseed.com/api/customer/clients",
     { cache: "no-store" }
   );
 
